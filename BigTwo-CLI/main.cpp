8acf2696cbdf7b10ecb127d224fddac280d229df
SheinH <chocolateshein.htike@gmail.com>
2018-12-29 15:07:12 -0500

    Initial Commit

---
+++ BigTwo-CLI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BigTwo-CLI
+//
+//  Created by Shein Htike on 12/29/18.
+//  Copyright © 2018 Shein Htike. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
